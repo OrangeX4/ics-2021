29bc3c138fbc633dc2123b3a7296b47494c508b7
tracer-ics2021 <tracer@njuics.org>
2021-09-15 20:45:18 +0800

    >  compile 201300035    Static hostname: orz          Icon name: computer-laptop            Chassis: laptop         Machine ID: a571b61377bb442e9b3fad100b7c407f            Boot ID: 3767f47171934fa88e4dcc4c93415a3c   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  20:45:18 up 10:02,  6 users,  load average: 2.18, 2.24, 2.13

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -78,7 +78,9 @@ static bool make_token(char *e) {
     nr_token = 0;
 
     while (e[position] != '\0') {
-        printf("position: %d\n", position);
+        if (position == 75) {
+            printf("position: %d\n", position);
+        }
         /* Try all rules one by one. */
         for (i = 0; i < NR_REGEX; i++) {
             if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 &&
