1c0d872fa9fd50af2ceb4f343b88f88a90affbae
tracer-ics2021 <tracer@njuics.org>
2021-09-15 19:34:42 +0800

    >  compile 201300035    Static hostname: orz          Icon name: computer-laptop            Chassis: laptop         Machine ID: a571b61377bb442e9b3fad100b7c407f            Boot ID: 3767f47171934fa88e4dcc4c93415a3c   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  19:34:42 up  8:51,  5 users,  load average: 2.45, 2.10, 1.93

--- nemu/include/debug.h
+++ nemu/include/debug.h
@@ -11,7 +11,7 @@
         __FILE__, __LINE__, __func__, ## __VA_ARGS__)
 #else
 #define Log(format, ...) \
-    _Log(ASNI_FMT("[%s:%d %s] " format, ASNI_FG_BLUE) "\n", \
+    _Log_to_file(ASNI_FMT("[%s:%d %s] " format, ASNI_FG_BLUE) "\n", \
         __FILE__, __LINE__, __func__, ## __VA_ARGS__)
 #endif
 
