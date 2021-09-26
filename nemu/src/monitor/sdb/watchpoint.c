137084ddfde5eb2fb485b5e478f016e9bd8adb81
tracer-ics2021 <tracer@njuics.org>
2021-09-26 17:13:51 +0800

    >  compile 201300035    Static hostname: orz          Icon name: computer-laptop            Chassis: laptop         Machine ID: a571b61377bb442e9b3fad100b7c407f            Boot ID: 0746750751d349b29c0c616e8fbdd485   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-36-generic       Architecture: x86-64  17:13:51 up  8:43,  5 users,  load average: 1.87, 1.93, 1.93

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -134,7 +134,7 @@ bool is_stop() {
         if (success) {
             if (current->next->value != value) {
                 result = true;
-                printf("Value of expr [%s] changed from %d to %d.",
+                printf("Value of expr [%s] changed from %d to %d.\n",
                        current->next->expr, current->next->value, value);
                 current->next->value = value;
             }
