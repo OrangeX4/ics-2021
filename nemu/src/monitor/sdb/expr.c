0ba56fffadfac8436405321f865facded0ebf2a8
tracer-ics2021 <tracer@njuics.org>
2021-09-26 09:52:09 +0800

    >  compile 201300035    Static hostname: orz          Icon name: computer-laptop            Chassis: laptop         Machine ID: a571b61377bb442e9b3fad100b7c407f            Boot ID: 0746750751d349b29c0c616e8fbdd485   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-36-generic       Architecture: x86-64  09:52:09 up  1:21,  5 users,  load average: 2.15, 2.02, 1.91

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -50,7 +50,7 @@ static struct rule {
     {"==", TK_EQ},                  // equal
     {"!=", TK_NEQ},                 // not equal
     {"&&", TK_AND},                 // and
-    {"||", TK_OR},                  // or
+    {"\\|\\|", TK_OR},                  // or
 };
 
 #define NR_REGEX ARRLEN(rules)
