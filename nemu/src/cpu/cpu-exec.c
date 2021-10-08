27dadb22a9b348f186666bd325011e1b8b611486
201300035-Fang Shengjun <201300035@smail.nju.edu.cn>
2021-10-08 22:44:39 +0800

    Fix conflicts

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -42,10 +42,13 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
         nemu_state.state = NEMU_STOP;
     }
 #ifdef CONFIG_ITRACE_COND
-  if (ITRACE_COND) log_write("%s\n", _this->logbuf);
+    //   if (ITRACE_COND) log_write("%s\n", _this->logbuf);
+    log_write("%s\n", _this->logbuf);
 #endif
-  if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
-  IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
+    if (g_print_step) {
+        IFDEF(CONFIG_ITRACE, puts(_this->logbuf));
+    }
+    IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
 }
 
 #include <isa-exec.h>
