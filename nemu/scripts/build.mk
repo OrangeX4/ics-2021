b64b3f87e78ea43cfe57f9185eb188df081a9104
tracer-ics2021 <tracer@njuics.org>
2021-10-20 15:16:02 +0800

    >  compile 201300035    Static hostname: orz          Icon name: computer-laptop            Chassis: laptop         Machine ID: a571b61377bb442e9b3fad100b7c407f            Boot ID: 7062baac260c4b0c9a671daf126f9219   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-38-generic       Architecture: x86-64  15:16:02 up  1:22,  4 users,  load average: 1.34, 1.47, 1.45

--- nemu/scripts/build.mk
+++ nemu/scripts/build.mk
@@ -32,9 +32,7 @@ $(OBJ_DIR)/%.o: %.c
 	@echo + CC $<
 	@mkdir -p $(dir $@)
 	@$(CC) $(CFLAGS) -c -o $@ $<
-	@$(CC) $(CFLAGS) -E -MF /dev/null $< | \
-			grep -ve '^#' | \
-			clang-format - > $(basename $@).i
+	@$(CC) $(CFLAGS) -E -MF
 	$(call call_fixdep, $(@:.o=.d), $@)
 
 $(OBJ_DIR)/%.o: %.cc
