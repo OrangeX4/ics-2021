76b1fb4ed4b069b8f6516392a7c7d339884250ec
201300035-Fang Shengjun <201300035@smail.nju.edu.cn>
2021-10-21 14:48:51 +0800

    Complete PA 2.1

--- nemu/src/isa/riscv32/instr/jump.h
+++ nemu/src/isa/riscv32/instr/jump.h
@@ -1,21 +1,21 @@
 def_EHelper(jal) {
-//   rtl_li(s, ddest, s->pc + 4);
-  rtl_li(s, ddest, s->pc);
-  rtl_addi(s, ddest, ddest, 4);
-//   rtl_j(s, s->pc + id_src1->imm);
-  rtl_li(s, s0, s->pc);
-  rtl_addi(s, s0, s0, id_src1->imm);
-  rtl_jr(s, s0);
+    //   rtl_li(s, ddest, s->pc + 4);
+    rtl_li(s, ddest, s->pc);
+    rtl_addi(s, ddest, ddest, 4);
+    //   rtl_j(s, s->pc + id_src1->imm);
+    rtl_li(s, s0, s->pc);
+    rtl_addi(s, s0, s0, id_src1->imm);
+    rtl_jr(s, s0);
 }
 
 def_EHelper(jalr) {
-//   rtl_li(s, ddest, s->pc + 4);
-  rtl_li(s, ddest, s->pc);
-  rtl_addi(s, ddest, ddest, 4);
-//   rtl_j(s, (*dsrc1 + id_src2->imm) & ~1);
-  rtl_li(s, s0, ~1);
-  rtl_mv(s, s1, dsrc1);
-  rtl_addi(s, s1, s1, id_src2->imm);
-  rtl_and(s, s0, s0, s1);
-  rtl_jr(s, s0);
+    //   rtl_li(s, ddest, s->pc + 4);
+    rtl_li(s, ddest, s->pc);
+    rtl_addi(s, ddest, ddest, 4);
+    //   rtl_j(s, (*dsrc1 + id_src2->imm) & ~1);
+    rtl_li(s, s0, ~1);
+    rtl_mv(s, s1, dsrc1);
+    rtl_addi(s, s1, s1, id_src2->imm);
+    rtl_and(s, s0, s0, s1);
+    rtl_jr(s, s0);
 }
