45ede4ef1279c1ac7f62ab6ba68986bcdf58a125
tracer-ics2021 <tracer@njuics.org>
2021-10-21 14:18:18 +0800

    >  compile 201300035    Static hostname: orz          Icon name: computer-laptop            Chassis: laptop         Machine ID: a571b61377bb442e9b3fad100b7c407f            Boot ID: 083af11898944073992ca7424d9d8e66   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-38-generic       Architecture: x86-64  14:18:18 up 30 min,  4 users,  load average: 2.34, 2.24, 1.78

--- nemu/src/isa/riscv32/instr/jump.h
+++ nemu/src/isa/riscv32/instr/jump.h
@@ -3,9 +3,9 @@ def_EHelper(jal) {
   rtl_li(s, ddest, s->pc);
   rtl_addi(s, ddest, ddest, 4);
 //   printf("imm: %d\n", id_src1->imm);
-  rtl_j(s, s->pc + id_src1->imm);
-//   rtl_mv(s, s0, s->pc);
-//   rtl_addi(s, s0, id_src1->imm);
+//   rtl_j(s, s->pc + id_src1->imm);
+  rtl_li(s, s0, s->pc);
+  rtl_addi(s, s0, s0, id_src1->imm);
 }
 
 def_EHelper(jalr) {
