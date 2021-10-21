f4229fdd88de3809cf57c85841097b6595339945
tracer-ics2021 <tracer@njuics.org>
2021-10-21 14:16:49 +0800

    >  compile 201300035    Static hostname: orz          Icon name: computer-laptop            Chassis: laptop         Machine ID: a571b61377bb442e9b3fad100b7c407f            Boot ID: 083af11898944073992ca7424d9d8e66   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-38-generic       Architecture: x86-64  14:16:49 up 29 min,  4 users,  load average: 1.99, 2.08, 1.68

--- nemu/src/isa/riscv32/instr/jump.h
+++ nemu/src/isa/riscv32/instr/jump.h
@@ -4,14 +4,18 @@ def_EHelper(jal) {
   rtl_addi(s, ddest, ddest, 4);
 //   printf("imm: %d\n", id_src1->imm);
   rtl_j(s, s->pc + id_src1->imm);
+//   rtl_mv(s, s0, s->pc);
+//   rtl_addi(s, s0, id_src1->imm);
 }
 
 def_EHelper(jalr) {
-  rtl_li(s, ddest, s->pc + 4);
-//   printf("imm: %d\n", id_src1->imm);
-  rtl_li(s, s0, ~1);
-  rtl_mv(s, s1, dsrc1);
-  rtl_addi(s, s1, s1, id_src2->imm);
-  rtl_and(s, s0, s0, s1);
-  rtl_jr(s, s0);
+//   rtl_li(s, ddest, s->pc + 4);
+//   rtl_li(s, ddest, s->pc);
+//   rtl_addi(s, ddest, ddest, 4);
+// //   printf("imm: %d\n", id_src1->imm);
+//   rtl_li(s, s0, ~1);
+//   rtl_mv(s, s1, dsrc1);
+//   rtl_addi(s, s1, s1, id_src2->imm);
+//   rtl_and(s, s0, s0, s1);
+//   rtl_jr(s, s0);
 }
