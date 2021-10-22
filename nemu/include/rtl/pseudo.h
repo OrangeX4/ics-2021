ee4fa3e928057d61e673cd0116bdebc98a1ec3c0
201300035-Fang Shengjun <201300035@smail.nju.edu.cn>
2021-10-22 08:59:39 +0800

    Complete pseudo.h

--- nemu/include/rtl/pseudo.h
+++ nemu/include/rtl/pseudo.h
@@ -17,33 +17,28 @@ static inline def_rtl(mv, rtlreg_t* dest, const rtlreg_t* src1) {
 
 static inline def_rtl(not, rtlreg_t* dest, const rtlreg_t* src1) {
     // dest <- ~src1
-    // TODO();
     rtl_xori(s, dest, src1, -1);
 }
 
 static inline def_rtl(neg, rtlreg_t* dest, const rtlreg_t* src1) {
     // dest <- -src1
-    //   TODO();
     rtl_sub(s, dest, rz, src1);
 }
 
 static inline def_rtl(sext, rtlreg_t* dest, const rtlreg_t* src1, int width) {
     // dest <- signext(src1[(width * 8 - 1) .. 0])
-    // TODO();
     rtl_slli(s, dest, src1, 32 - width * 8);
     rtl_srai(s, dest, dest, 32 - width * 8);
 }
 
 static inline def_rtl(zext, rtlreg_t* dest, const rtlreg_t* src1, int width) {
     // dest <- zeroext(src1[(width * 8 - 1) .. 0])
-    // TODO();
     rtl_slli(s, dest, src1, 32 - width * 8);
     rtl_srli(s, dest, dest, 32 - width * 8);
 }
 
 static inline def_rtl(msb, rtlreg_t* dest, const rtlreg_t* src1, int width) {
     // dest <- src1[width * 8 - 1]
-    // TODO();
     rtl_zext(s, dest, src1, width);
     rtl_srli(s, dest, dest, width * 8 - 1);
 }
