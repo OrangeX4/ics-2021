287243d8efa6f469e12457309ca69658142f03e7
tracer-ics2021 <tracer@njuics.org>
2021-10-22 20:24:27 +0800

    >  compile 201300035    Static hostname: orz          Icon name: computer-laptop            Chassis: laptop         Machine ID: a571b61377bb442e9b3fad100b7c407f            Boot ID: e07aa6fc52d947eb90ca796d7e7ba940   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-38-generic       Architecture: x86-64  20:24:27 up  4:30,  3 users,  load average: 1.87, 1.64, 1.64

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -170,7 +170,7 @@ void cpu_exec(uint64_t n) {
             for (int i = 0; i < MAX_IRINGBUF_LENGTH - 1; ++i) {
                 printf("    %s\n", iringbuf[(iringbuf_count + i) % MAX_IRINGBUF_LENGTH]);
             }
-            printf("--> %s\n", iringbuf[(iringbuf_count + 7) % MAX_IRINGBUF_LENGTH]);
+            printf("--> %s\n\n\n", iringbuf[(iringbuf_count + 7) % MAX_IRINGBUF_LENGTH]);
             // fall through
         case NEMU_QUIT:
             statistic();
