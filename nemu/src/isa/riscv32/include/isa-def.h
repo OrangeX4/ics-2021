5e024a42a7dad34fc72995465cc944aaa7802932
tracer-ics2021 <tracer@njuics.org>
2021-10-22 10:35:29 +0800

    >  compile 201300035    Static hostname: orz          Icon name: computer-laptop            Chassis: laptop         Machine ID: a571b61377bb442e9b3fad100b7c407f            Boot ID: 404740e03ac1446d80e77cfccce73c06   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-38-generic       Architecture: x86-64  10:35:30 up 24 min,  4 users,  load average: 1.84, 1.92, 1.52

--- nemu/src/isa/riscv32/include/isa-def.h
+++ nemu/src/isa/riscv32/include/isa-def.h
@@ -29,7 +29,10 @@ typedef struct {
       uint32_t rd        : 5;
       uint32_t funct3    : 3;
       uint32_t rs1       : 5;
-      int32_t  simm11_0  :12;
+      union {
+        int32_t  simm11_0  :12;
+        uint32_t  imm11_0  :12;
+      };
     } i;
     struct {
       uint32_t opcode1_0 : 2;
