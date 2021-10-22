c7d8f1242962b73199dddc32bd48e8bf80d143d5
tracer-ics2021 <tracer@njuics.org>
2021-10-22 14:36:06 +0800

    >  compile 201300035    Static hostname: orz          Icon name: computer-laptop            Chassis: laptop         Machine ID: a571b61377bb442e9b3fad100b7c407f            Boot ID: 67296eace10749f49c38087423ec429a   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-38-generic       Architecture: x86-64  14:36:06 up 46 min,  4 users,  load average: 3.57, 2.47, 2.12

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -6,7 +6,6 @@
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
     if (cpu.pc != pc) return false;
     for (int i = 0; i < 32; ++i) {
-        printf("%d == %d; ", cpu.gpr[i]._32, ref_r->gpr[i]._32);
         if (cpu.gpr[i]._32 != ref_r->gpr[i]._32) return false; 
     }
     return true;
