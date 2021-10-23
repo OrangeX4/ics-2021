9c8ba5a1bd17e85b41b6bc154c699db377adce77
tracer-ics2021 <tracer@njuics.org>
2021-10-23 16:34:57 +0800

    >  compile 201300035    Static hostname: orz          Icon name: computer-laptop            Chassis: laptop         Machine ID: a571b61377bb442e9b3fad100b7c407f            Boot ID: ffe693420e4546f4a7786476b08400b3   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  16:34:57 up  6:06,  4 users,  load average: 1.61, 1.73, 1.61

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -19,9 +19,6 @@ static word_t pmem_read(paddr_t addr, int len) {
 
 static void pmem_write(paddr_t addr, int len, word_t data) {
   host_write(guest_to_host(addr), len, data);
-#ifdef CONFIG_MTRACE
-  log_write("pmem_write(%x, %d, %u)\n", addr, len, data);
-#endif
 }
 
 void init_mem() {
@@ -41,6 +38,9 @@ void init_mem() {
 }
 
 word_t paddr_read(paddr_t addr, int len) {
+#ifdef CONFIG_MTRACE
+  log_write("paddr_read(%x, %d) = %u\n", addr, len, pmem_read(addr, len));
+#endif
   if (likely(in_pmem(addr))) return pmem_read(addr, len);
   MUXDEF(CONFIG_DEVICE, return mmio_read(addr, len),
     panic("address = " FMT_PADDR " is out of bound of pmem [" FMT_PADDR ", " FMT_PADDR ") at pc = " FMT_WORD,
@@ -48,6 +48,9 @@ word_t paddr_read(paddr_t addr, int len) {
 }
 
 void paddr_write(paddr_t addr, int len, word_t data) {
+#ifdef CONFIG_MTRACE
+  log_write("paddr_write(%x, %d, %u)\n", addr, len, data);
+#endif
   if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
   MUXDEF(CONFIG_DEVICE, mmio_write(addr, len, data),
     panic("address = " FMT_PADDR " is out of bound of pmem [" FMT_PADDR ", " FMT_PADDR ") at pc = " FMT_WORD,
