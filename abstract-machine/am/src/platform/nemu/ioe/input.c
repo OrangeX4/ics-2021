21f28753831f680201901e3f6d771a2923a4a1c3
tracer-ics2021 <tracer@njuics.org>
2021-10-23 22:24:31 +0800

    >  compile 201300035    Static hostname: orz          Icon name: computer-laptop            Chassis: laptop         Machine ID: a571b61377bb442e9b3fad100b7c407f            Boot ID: ffe693420e4546f4a7786476b08400b3   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  22:24:31 up 11:55,  4 users,  load average: 2.18, 1.96, 1.77

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -4,8 +4,9 @@
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  kbd->keycode = inl(KBD_ADDR) & ~KEYDOWN_MASK;
+  uint32_t key = inl(KBD_ADDR);
+  kbd->keycode = key & ~KEYDOWN_MASK;
 //   kbd->keydown = kbd->keycode & KEYDOWN_MASK;
-  kbd->keydown = inl(KBD_ADDR) & KEYDOWN_MASK;
+  kbd->keydown = key & KEYDOWN_MASK;
 //   kbd->keycode = AM_KEY_NONE;
 }
