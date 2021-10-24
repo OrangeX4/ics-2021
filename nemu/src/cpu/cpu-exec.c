f028177387ba8241f2e897c78be7a83d51721a64
tracer-ics2021 <tracer@njuics.org>
2021-10-24 19:45:21 +0800

    >  compile 201300035    Static hostname: orz          Icon name: computer-laptop            Chassis: laptop         Machine ID: a571b61377bb442e9b3fad100b7c407f            Boot ID: 006b3d987b0e4d8b8fbf63a81be68204   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  19:45:21 up  9:29,  4 users,  load average: 1.69, 1.99, 1.97

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -28,6 +28,7 @@ buf iringbuf[MAX_IRINGBUF_LENGTH];
 uint32_t iringbuf_count = 0;
 
 void device_update();
+void ftrace(Decode *s);
 
 // #ifdef CONFIG_DEBUG
 // static void debug_hook(vaddr_t pc, const char *asmbuf) {
@@ -43,10 +44,6 @@ void device_update();
 // }
 // #endif
 
-void init_ftrace(const char* elf_file) {
-    printf("success\n");
-}
-
 static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
     // Watchpoint
     if (is_stop()) {
@@ -56,6 +53,9 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
     // there is no ITRACE_COND
     //   if (ITRACE_COND) log_write("%s\n", _this->logbuf);
     log_write("%s\n", _this->logbuf);
+#endif
+#ifdef CONFIG_FTRACE
+    ftrace(_this);
 #endif
     if (g_print_step) {
         IFDEF(CONFIG_ITRACE, puts(_this->logbuf));
---
+++ nemu/src/cpu/ftrace.c
@@ -0,0 +1,80 @@
+#include <isa.h>
+#include <elf.h>
+#include <isa-all-instr.h>
+
+#define RET_INST 0x00008067
+
+typedef struct {
+    char name[64];
+    vaddr_t addr;
+    vaddr_t end;
+} Functab;
+
+Functab functab[64];
+int func_length = 0;
+static char *elf_file;
+
+void init_ftrace(char *file) {
+
+    elf_file = file;
+    if (elf_file == NULL) return;
+
+    FILE *fp = fopen(elf_file, "r");
+    Elf32_Ehdr section_table = {};
+    assert(fread(&section_table, sizeof(Elf32_Ehdr), 1, fp));
+
+    // get the section table
+    fseek(fp, section_table.e_shoff, SEEK_SET);
+    // string section
+    Elf32_Shdr string_section = {};
+    // symbol section
+    Elf32_Shdr symbol_section = {};
+
+    // temp section
+    Elf32_Shdr temp_section = {};
+    for (Elf32_Half i = 0; i < section_table.e_shnum; ++i) {
+        assert(fread(&temp_section, sizeof(Elf32_Shdr), 1, fp));
+        // exclude the string section for section table
+        if (temp_section.sh_type == SHT_STRTAB && i != section_table.e_shstrndx) {
+            string_section = temp_section;
+        } else if (temp_section.sh_type == SHT_SYMTAB) {
+            symbol_section = temp_section;
+        }
+    }
+
+    Elf32_Sym symbol = {};
+    for (Elf32_Half i = 0; i < symbol_section.sh_size / symbol_section.sh_entsize; ++i) {
+        fseek(fp, symbol_section.sh_offset + i * symbol_section.sh_entsize, SEEK_SET);
+        assert(fread(&symbol, sizeof(Elf32_Sym), 1, fp));
+
+        if (ELF32_ST_TYPE(symbol.st_info) == STT_FUNC) {
+            fseek(fp, string_section.sh_offset + symbol.st_name, SEEK_SET);
+            assert(fgets(functab[func_length].name, 63, fp));
+            functab[func_length].addr = symbol.st_value;
+            functab[func_length].end = symbol.st_value + symbol.st_size;
+            ++func_length;
+        }
+    }
+    
+    fclose(fp);
+}
+
+void ftrace(Decode *s) {
+    if (elf_file == NULL) return;
+
+    vaddr_t addr = s->dnpc;
+    if (s->isa.instr.val == RET_INST) {
+        for (int i = func_length - 1; i >= 0; ++i) {
+            if (addr >= functab[i].addr && addr <= functab[i].end) {
+                log_write("[ftrace] ret %s\n", functab[i].name);
+                break;
+            }
+        }
+    } else {
+        for (int i = 0; i < func_length; ++i) {
+            if (addr == functab[i].addr) {
+                log_write("[ftrace] call %s@" FMT_PADDR "]\n", functab[i].name, functab[i].addr);
+            }
+        }
+    }
+}
