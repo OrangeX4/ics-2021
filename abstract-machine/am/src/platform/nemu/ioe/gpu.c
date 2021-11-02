2fac22177e3c833b173d9cb39f67115ee2e9b80d
tracer-ics2021 <tracer@njuics.org>
2021-11-02 16:06:33 +0800

    >  compile 201300035    Static hostname: orz          Icon name: computer-laptop            Chassis: laptop         Machine ID: a571b61377bb442e9b3fad100b7c407f            Boot ID: 3cf4c93804cf4acca31f8fbe3946f9fe   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  16:06:33 up 34 min,  3 users,  load average: 2.24, 2.09, 1.97

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -14,27 +14,27 @@ void __am_gpu_init() {
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
-  uint32_t hw = inl(VGACTL_ADDR);
-  *cfg = (AM_GPU_CONFIG_T) {
-    .present = true, .has_accel = false,
-    .width = (hw >> 16), .height = ((hw << 16) >> 16),
-    .vmemsz = 0
-  };
+//   uint32_t hw = inl(VGACTL_ADDR);
+//   *cfg = (AM_GPU_CONFIG_T) {
+//     .present = true, .has_accel = false,
+//     .width = (hw >> 16), .height = ((hw << 16) >> 16),
+//     .vmemsz = 0
+//   };
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-  uint32_t hw = inl(VGACTL_ADDR);
-  int i, j;
-  int w = (hw >> 16);
-  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  for (i = 0; i < ctl->h; ++i) {
-    for (j = 0; j < ctl->w; ++j) {
-      fb[(ctl->y + i) * w + (ctl->x + j)] = ((uint32_t *)ctl->pixels)[i * ctl->w + j];
-    }
-  }
-  if (ctl->sync) {
-    outl(SYNC_ADDR, 1);
-  }
+//   uint32_t hw = inl(VGACTL_ADDR);
+//   int i, j;
+//   int w = (hw >> 16);
+//   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+//   for (i = 0; i < ctl->h; ++i) {
+//     for (j = 0; j < ctl->w; ++j) {
+//       fb[(ctl->y + i) * w + (ctl->x + j)] = ((uint32_t *)ctl->pixels)[i * ctl->w + j];
+//     }
+//   }
+//   if (ctl->sync) {
+//     outl(SYNC_ADDR, 1);
+//   }
 }
 
 void __am_gpu_status(AM_GPU_STATUS_T *status) {
