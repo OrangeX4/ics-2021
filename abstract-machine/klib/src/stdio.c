c360a369a0f07ab0a97fddc7d278f8f6f00e2653
tracer-ics2021 <tracer@njuics.org>
2021-11-02 16:22:13 +0800

    >  compile 201300035    Static hostname: orz          Icon name: computer-laptop            Chassis: laptop         Machine ID: a571b61377bb442e9b3fad100b7c407f            Boot ID: 3cf4c93804cf4acca31f8fbe3946f9fe   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  16:22:13 up 50 min,  3 users,  load average: 2.83, 2.50, 2.34

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,223 +5,244 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-void itoa(unsigned int n, char *buf) {
-    int i;
+// void itoa(unsigned int n, char *buf) {
+//     int i;
 
-    if (n < 10) {
-        buf[0] = n + '0';
-        buf[1] = '\0';
-        return;
-    }
-    itoa(n / 10, buf);
+//     if (n < 10) {
+//         buf[0] = n + '0';
+//         buf[1] = '\0';
+//         return;
+//     }
+//     itoa(n / 10, buf);
 
-    for (i = 0; buf[i] != '\0'; i++)
-        ;
+//     for (i = 0; buf[i] != '\0'; i++)
+//         ;
 
-    buf[i] = (n % 10) + '0';
+//     buf[i] = (n % 10) + '0';
 
-    buf[i + 1] = '\0';
-}
+//     buf[i + 1] = '\0';
+// }
 
-void xtoa(unsigned int n, char *buf) {
-    int i;
+// void xtoa(unsigned int n, char *buf) {
+//     int i;
 
-    if (n < 16) {
-        if (n < 10) {
-            buf[0] = n + '0';
-        } else {
-            buf[0] = n - 10 + 'a';
-        }
-        buf[1] = '\0';
-        return;
-    }
-    xtoa(n / 16, buf);
+//     if (n < 16) {
+//         if (n < 10) {
+//             buf[0] = n + '0';
+//         } else {
+//             buf[0] = n - 10 + 'a';
+//         }
+//         buf[1] = '\0';
+//         return;
+//     }
+//     xtoa(n / 16, buf);
 
-    for (i = 0; buf[i] != '\0'; i++)
-        ;
+//     for (i = 0; buf[i] != '\0'; i++)
+//         ;
 
-    if ((n % 16) < 10) {
-        buf[i] = (n % 16) + '0';
-    } else {
-        buf[i] = (n % 16) - 10 + 'a';
-    }
-    buf[i + 1] = '\0';
-}
+//     if ((n % 16) < 10) {
+//         buf[i] = (n % 16) + '0';
+//     } else {
+//         buf[i] = (n % 16) - 10 + 'a';
+//     }
+//     buf[i + 1] = '\0';
+// }
 
-int is_digit(unsigned char c) {
-    if (c >= '0' && c <= '9')
-        return 1;
-    else
-        return 0;
-}
+// int is_digit(unsigned char c) {
+//     if (c >= '0' && c <= '9')
+//         return 1;
+//     else
+//         return 0;
+// }
 
-int is_letter(unsigned char c) {
-    if (c >= 'a' && c <= 'z')
-        return 1;
-    else if (c >= 'A' && c <= 'Z')
-        return 1;
-    else
-        return 0;
-}
+// int is_letter(unsigned char c) {
+//     if (c >= 'a' && c <= 'z')
+//         return 1;
+//     else if (c >= 'A' && c <= 'Z')
+//         return 1;
+//     else
+//         return 0;
+// }
+
+// int printf(const char *fmt, ...) {
+//     va_list ap;
+//     va_start(ap, fmt);
+
+//     int count = 0;
+//     char c;
+//     char *s;
+//     int n;
+
+//     char buf[65];
+//     char digit[16];
+
+//     memset(buf, 0, sizeof(buf));
+//     memset(digit, 0, sizeof(digit));
+
+//     while (*fmt != '\0') {
+//         if (*fmt == '%') {
+//             fmt++;
+//             switch (*fmt) {
+//                 case 'd': {
+//                     n = va_arg(ap, int);
+//                     if (n < 0) {
+//                         putch('-');
+//                         n = -n;
+//                     }
+//                     itoa(n, buf);
+//                     printf(buf);
+//                     break;
+//                 }
+//                 case 'c': {
+//                     c = va_arg(ap, int);
+//                     putch(c);
+
+//                     break;
+//                 }
+//                 case 'x': {
+//                     n = va_arg(ap, int);
+//                     xtoa(n, buf);
+//                     printf(buf);
+//                     break;
+//                 }
+//                 case 's': {
+//                     s = va_arg(ap, char *);
+//                     printf(s);
+//                     break;
+//                 }
+//                 case '%': {
+//                     putch('%');
+//                     break;
+//                 }
+//                 default:
+//                     break;
+//             }
+//         } else {
+//             putch(*fmt);
+//         }
+//         fmt++;
+//     }
+
+//     va_end(ap);
+//     return count;
+// }
+
+
+// int vsnprintf(char *out, size_t _n, const char *fmt, va_list ap) {
+//     int count = 0;
+//     char c;
+//     char *s;
+//     int n;
+
+//     char *_out = out;
+
+//     char buf[65];
+//     char digit[16];
+
+//     memset(buf, 0, sizeof(buf));
+//     memset(digit, 0, sizeof(digit));
+
+//     while (*fmt != '\0' && (out - _out) < _n) {
+//         if (*fmt == '%') {
+//             fmt++;
+//             switch (*fmt) {
+//                 case 'd': {
+//                     n = va_arg(ap, int);
+//                     if (n < 0) {
+//                         *out = '-';
+//                         out++;
+//                         n = -n;
+//                     }
+//                     itoa(n, buf);
+//                     memcpy(out, buf, strlen(buf));
+//                     out += strlen(buf);
+//                     break;
+//                 }
+//                 case 'c': {
+//                     c = va_arg(ap, int);
+//                     *out = c;
+//                     out++;
+
+//                     break;
+//                 }
+//                 case 'x': {
+//                     n = va_arg(ap, int);
+//                     xtoa(n, buf);
+//                     memcpy(out, buf, strlen(buf));
+//                     out += strlen(buf);
+//                     break;
+//                 }
+//                 case 's': {
+//                     s = va_arg(ap, char *);
+//                     memcpy(out, s, strlen(s));
+//                     out += strlen(s);
+//                     break;
+//                 }
+//                 case '%': {
+//                     *out = '%';
+//                     out++;
+//                     break;
+//                 }
+//                 default:
+//                     break;
+//             }
+//         } else {
+//             *out = *fmt;
+//             out++;
+//         }
+//         fmt++;
+//     }
+//     *out = '\0';
+//     return count;
+// }
+
+// int snprintf(char *out, size_t n, const char *fmt, ...) {
+//     va_list ap;
+//     va_start(ap, fmt);
+
+//     int count = 0;
+//     count = vsnprintf(out, n, fmt, ap);
+
+//     va_end(ap);
+
+//     return count;
+// }
+
+// int vsprintf(char *out, const char *fmt, va_list ap) {
+//     return vsnprintf(out, -1, fmt, ap);
+// }
+
+// int sprintf(char *out, const char *fmt, ...) {
+//     va_list ap;
+//     va_start(ap, fmt);
+
+//     int count = 0;
+//     count = vsprintf(out, fmt, ap);
+
+//     va_end(ap);
+
+//     return count;
+// }
+
 
 int printf(const char *fmt, ...) {
-    va_list ap;
-    va_start(ap, fmt);
-
-    int count = 0;
-    char c;
-    char *s;
-    int n;
-
-    char buf[65];
-    char digit[16];
-
-    memset(buf, 0, sizeof(buf));
-    memset(digit, 0, sizeof(digit));
-
-    while (*fmt != '\0') {
-        if (*fmt == '%') {
-            fmt++;
-            switch (*fmt) {
-                case 'd': {
-                    n = va_arg(ap, int);
-                    if (n < 0) {
-                        putch('-');
-                        n = -n;
-                    }
-                    itoa(n, buf);
-                    printf(buf);
-                    break;
-                }
-                case 'c': {
-                    c = va_arg(ap, int);
-                    putch(c);
-
-                    break;
-                }
-                case 'x': {
-                    n = va_arg(ap, int);
-                    xtoa(n, buf);
-                    printf(buf);
-                    break;
-                }
-                case 's': {
-                    s = va_arg(ap, char *);
-                    printf(s);
-                    break;
-                }
-                case '%': {
-                    putch('%');
-                    break;
-                }
-                default:
-                    break;
-            }
-        } else {
-            putch(*fmt);
-        }
-        fmt++;
-    }
-
-    va_end(ap);
-    return count;
-}
-
-
-int vsnprintf(char *out, size_t _n, const char *fmt, va_list ap) {
-    int count = 0;
-    char c;
-    char *s;
-    int n;
-
-    char *_out = out;
-
-    char buf[65];
-    char digit[16];
-
-    memset(buf, 0, sizeof(buf));
-    memset(digit, 0, sizeof(digit));
-
-    while (*fmt != '\0' && (out - _out) < _n) {
-        if (*fmt == '%') {
-            fmt++;
-            switch (*fmt) {
-                case 'd': {
-                    n = va_arg(ap, int);
-                    if (n < 0) {
-                        *out = '-';
-                        out++;
-                        n = -n;
-                    }
-                    itoa(n, buf);
-                    memcpy(out, buf, strlen(buf));
-                    out += strlen(buf);
-                    break;
-                }
-                case 'c': {
-                    c = va_arg(ap, int);
-                    *out = c;
-                    out++;
-
-                    break;
-                }
-                case 'x': {
-                    n = va_arg(ap, int);
-                    xtoa(n, buf);
-                    memcpy(out, buf, strlen(buf));
-                    out += strlen(buf);
-                    break;
-                }
-                case 's': {
-                    s = va_arg(ap, char *);
-                    memcpy(out, s, strlen(s));
-                    out += strlen(s);
-                    break;
-                }
-                case '%': {
-                    *out = '%';
-                    out++;
-                    break;
-                }
-                default:
-                    break;
-            }
-        } else {
-            *out = *fmt;
-            out++;
-        }
-        fmt++;
-    }
-    *out = '\0';
-    return count;
-}
-
-int snprintf(char *out, size_t n, const char *fmt, ...) {
-    va_list ap;
-    va_start(ap, fmt);
-
-    int count = 0;
-    count = vsnprintf(out, n, fmt, ap);
-
-    va_end(ap);
-
-    return count;
+  panic("Not implemented");
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-    return vsnprintf(out, -1, fmt, ap);
+  panic("Not implemented");
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-    va_list ap;
-    va_start(ap, fmt);
+  panic("Not implemented");
+}
 
-    int count = 0;
-    count = vsprintf(out, fmt, ap);
+int snprintf(char *out, size_t n, const char *fmt, ...) {
+  panic("Not implemented");
+}
 
-    va_end(ap);
-
-    return count;
+int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
+  panic("Not implemented");
 }
 
 #endif
--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -32,25 +32,32 @@ int atoi(const char *nptr) {
 //     Area *next;
 // };
 
-void *addr = NULL;
+// void *addr = NULL;
+
+// void *malloc(size_t size) {
+//     // On native, malloc() will be called during initialization of C runtime.
+//     // Therefore do not call panic() here, else it will yield a dead recursion:
+//     //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
+//     #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
+//         panic("Not implemented");
+//     #endif
+//         return NULL;
+//     // if (addr == NULL) addr = heap.start;
+//     // size  = (size_t)ROUNDUP(size, 8);
+//     // char *old = addr;
+//     // addr += size;
+//     // for (uint64_t *p = (uint64_t *)old; p != (uint64_t *)addr; p ++) {
+//     //     *p = 0;
+//     // }
+//     // return old;
+// }
+// void free(void *ptr) {}
 
 void *malloc(size_t size) {
-    // On native, malloc() will be called during initialization of C runtime.
-    // Therefore do not call panic() here, else it will yield a dead recursion:
-    //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
-    // #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-    //     panic("Not implemented");
-    // #endif
-    //     return NULL;
-    if (addr == NULL) addr = heap.start;
-    size  = (size_t)ROUNDUP(size, 8);
-    char *old = addr;
-    addr += size;
-    for (uint64_t *p = (uint64_t *)old; p != (uint64_t *)addr; p ++) {
-        *p = 0;
-    }
-    return old;
+  panic("Not implemented");
 }
-void free(void *ptr) {}
+
+void free(void *ptr) {
+}
 
 #endif
