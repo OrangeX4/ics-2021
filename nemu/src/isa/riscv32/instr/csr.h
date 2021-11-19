2d66cc8375770871a9d1538d40b619aaa4c53b69
tracer-ics2021 <tracer@njuics.org>
2021-11-20 00:46:15 +0800

    >  compile 201300035    Static hostname: orz          Icon name: computer-laptop            Chassis: laptop         Machine ID: a571b61377bb442e9b3fad100b7c407f            Boot ID: c19fbb9ce8384e938fbbc0a4a4ab2ec9   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  00:46:15 up  8:26,  5 users,  load average: 2.00, 1.66, 1.49

--- nemu/src/isa/riscv32/instr/csr.h
+++ nemu/src/isa/riscv32/instr/csr.h
@@ -4,6 +4,12 @@ def_EHelper(csrrs) {
   rtl_mv(s, ddest, s0);
 }
 
+def_EHelper(csrrw) {
+  rtl_mv(s, s0, dsrc2);
+  rtl_mv(s, dsrc2, dsrc1);
+  rtl_mv(s, ddest, s0);
+}
+
 def_EHelper(ecall) {
   rtl_j(s, isa_raise_intr(gpr(17), cpu.pc));
 }
