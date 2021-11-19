f51c828735026ea65a6da0c08e68edd14a9a8cf0
tracer-ics2021 <tracer@njuics.org>
2021-11-20 01:38:26 +0800

    >  compile 201300035    Static hostname: orz          Icon name: computer-laptop            Chassis: laptop         Machine ID: a571b61377bb442e9b3fad100b7c407f            Boot ID: c19fbb9ce8384e938fbbc0a4a4ab2ec9   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  01:38:26 up  9:18,  5 users,  load average: 1.58, 1.58, 1.53

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -5,9 +5,9 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
      * Then return the address of the interrupt/exception vector.
      */
 
-    cpu.csr[3]._32 = cpu.pc;    // mepc
-    cpu.csr[1]._32 = NO;        // mcause
-    return cpu.csr[0]._32;      // mtvec
+    cpu.csr[3]._32 = cpu.pc + 4;    // mepc
+    cpu.csr[1]._32 = NO;            // mcause
+    return cpu.csr[0]._32;          // mtvec
 }
 
 word_t isa_query_intr() { return INTR_EMPTY; }
