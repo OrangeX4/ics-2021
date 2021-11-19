f8f64750cecf6db626d7e6fbee540ffd350ef628
201300035-Fang Shengjun <201300035@smail.nju.edu.cn>
2021-11-19 17:15:03 +0800

    Update cpp config

--- abstract-machine/am/include/am.h
+++ abstract-machine/am/include/am.h
@@ -6,6 +6,7 @@
 #include <stdbool.h>
 #include ARCH_H // this macro is defined in $CFLAGS
                 // examples: "arch/x86-qemu.h", "arch/native.h", ...
+// #include "arch/riscv32-nemu.h"
 
 // Memory protection flags
 #define MMAP_NONE  0x00000000 // no access
