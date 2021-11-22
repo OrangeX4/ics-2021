d0e924fc58ac3bc0f343a58ddaf58b1abe89051e
tracer-ics2021 <tracer@njuics.org>
2021-11-22 18:39:55 +0800

    >  compile 201300035    Static hostname: orz          Icon name: computer-laptop            Chassis: laptop         Machine ID: a571b61377bb442e9b3fad100b7c407f            Boot ID: 33980c041b1a481dbbd372b52e956df0   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  18:39:55 up 8 min,  3 users,  load average: 1.93, 1.53, 0.85

--- nemu/src/isa/riscv32/instr/jump.h
+++ nemu/src/isa/riscv32/instr/jump.h
@@ -10,14 +10,15 @@ def_EHelper(jal) {
 
 def_EHelper(jalr) {
     //   rtl_li(s, ddest, s->pc + 4);
-    rtl_li(s, ddest, s->pc);
-    rtl_addi(s, ddest, ddest, 4);
+    rtl_li(s, s0, s->pc);
+    rtl_addi(s, s0, s0, 4);
     //   rtl_j(s, (*dsrc1 + id_src2->imm) & ~1);
-    rtl_li(s, s0, ~1);
-    rtl_mv(s, s1, dsrc1);
-    rtl_addi(s, s1, s1, id_src2->imm);
-    rtl_and(s, s0, s0, s1);
-    rtl_jr(s, s0);
+    rtl_li(s, s1, ~1);
+    rtl_mv(s, s2, dsrc1);
+    rtl_addi(s, s2, s2, id_src2->simm);
+    rtl_and(s, s1, s1, s2);
+    rtl_mv(s, ddest, s0);
+    rtl_jr(s, s1);
 }
 
 static inline void jump_helper(Decode *s, uint32_t relop) {
