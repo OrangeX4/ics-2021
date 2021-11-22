c68e610d8ff8c4881abdfdfe3eb365f2d7d8432f
tracer-ics2021 <tracer@njuics.org>
2021-11-22 16:28:01 +0800

    >  compile 201300035    Static hostname: orz          Icon name: computer-laptop            Chassis: laptop         Machine ID: a571b61377bb442e9b3fad100b7c407f            Boot ID: 48d0e67c7fda4b0f9988dc9044472cd2   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  16:28:01 up 7 min,  4 users,  load average: 1.35, 1.24, 0.68

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -3,10 +3,21 @@
 
 #include "../local-include/reg.h"
 
+const char *regs[] = {"$0", "ra", "sp",  "gp",  "tp", "t0", "t1", "t2",
+                      "s0", "s1", "a0",  "a1",  "a2", "a3", "a4", "a5",
+                      "a6", "a7", "s2",  "s3",  "s4", "s5", "s6", "s7",
+                      "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"};
+
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-    if (cpu.pc != ref_r->pc) return false;
+    if (cpu.pc != ref_r->pc) {
+        printf("pc is different.\n");
+        return false;
+    }
     for (int i = 0; i < 32; ++i) {
-        if (cpu.gpr[i]._32 != ref_r->gpr[i]._32) return false;
+        if (cpu.gpr[i]._32 != ref_r->gpr[i]._32) {
+            printf("%s is different.\n", regs[i]);
+            return false;
+        }
     }
     // for (int i = 0; i < 4; ++i) {
     //     if (cpu.csr[i]._32 != ref_r->csr[i]._32) return false;
