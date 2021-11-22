6a3545249a227d1d1df3f8d504295aed3d0cf756
tracer-ics2021 <tracer@njuics.org>
2021-11-22 11:47:29 +0800

    >  compile 201300035    Static hostname: orz          Icon name: computer-laptop            Chassis: laptop         Machine ID: a571b61377bb442e9b3fad100b7c407f            Boot ID: 69feb433daac48728f03f781bd18d4c3   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  11:47:29 up  1:54,  4 users,  load average: 1.80, 1.80, 1.64

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,22 +1,27 @@
-#include <proc.h>
 #include <elf.h>
+#include <proc.h>
 
 #ifdef __LP64__
-# define Elf_Ehdr Elf64_Ehdr
-# define Elf_Phdr Elf64_Phdr
+#define Elf_Ehdr Elf64_Ehdr
+#define Elf_Phdr Elf64_Phdr
 #else
-# define Elf_Ehdr Elf32_Ehdr
-# define Elf_Phdr Elf32_Phdr
+#define Elf_Ehdr Elf32_Ehdr
+#define Elf_Phdr Elf32_Phdr
 #endif
 
+#define ELF_MAGIC 0x7f454c46
+
+size_t ramdisk_read(void *buf, size_t offset, size_t len);
+
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();
-  return 0;
+    Elf_Ehdr elf = {};
+    ramdisk_read(&elf, 0, sizeof(Elf_Ehdr));
+    assert(*(uint32_t *)elf.e_ident == ELF_MAGIC);
+    return 0;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
-  uintptr_t entry = loader(pcb, filename);
-  Log("Jump to entry = %p", entry);
-  ((void(*)())entry) ();
+    uintptr_t entry = loader(pcb, filename);
+    Log("Jump to entry = %p", entry);
+    ((void (*)())entry)();
 }
-
