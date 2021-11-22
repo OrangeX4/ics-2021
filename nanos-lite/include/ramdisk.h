1073af9d8a84bb2a88722e504d05821ff9bdf0b4
tracer-ics2021 <tracer@njuics.org>
2021-11-22 15:37:41 +0800

    >  compile 201300035    Static hostname: orz          Icon name: computer-laptop            Chassis: laptop         Machine ID: a571b61377bb442e9b3fad100b7c407f            Boot ID: d6f9af8582fd478fb883d1af6cc4aba4   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  15:37:41 up  1:42,  4 users,  load average: 2.00, 1.73, 1.45

--- nanos-lite/include/ramdisk.h
+++ nanos-lite/include/ramdisk.h
@@ -3,6 +3,10 @@
 
 #include <common.h>
 
+extern uint8_t ramdisk_start;
+extern uint8_t ramdisk_end;
+#define RAMDISK_SIZE ((&ramdisk_end) - (&ramdisk_start))
+
 size_t ramdisk_read(void *buf, size_t offset, size_t len);
 
 /* write `len' bytes starting from `buf' into the `offset' of ramdisk */
