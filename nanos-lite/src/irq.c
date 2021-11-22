467f96d80fce5af20a0819044795ed9e7276ce51
tracer-ics2021 <tracer@njuics.org>
2021-11-22 21:49:58 +0800

    >  compile 201300035    Static hostname: orz          Icon name: computer-laptop            Chassis: laptop         Machine ID: a571b61377bb442e9b3fad100b7c407f            Boot ID: 20edccbe52d249a59bb0b1a3e7130583   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  21:49:59 up 12 min,  4 users,  load average: 1.79, 1.59, 1.02

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -2,8 +2,8 @@
 
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
-    case EVENT_YIELD: printf("EVENT_YIELD, cause: %d, ref: %d\n", e.cause, e.ref); break;
-    default: panic("Unhandled event ID = %d, cause: %d, ref: %d\n", e.event, e.cause, e.ref);
+    case EVENT_YIELD: printf("EVENT_YIELD, cause: %d, ref: %d, GPR1: %d\n", e.cause, e.ref, c->GPR1); break;
+    default: panic("Unhandled event ID = %d, cause: %d, ref: %d, GPR1: %d\n", e.event, e.cause, e.ref, c->GPR1);
   }
 
   return c;
