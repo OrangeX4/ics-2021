f99e2be8d1c26e01b5106a8dd0a7d158983f88f0
tracer-ics2021 <tracer@njuics.org>
2021-11-24 23:38:27 +0800

    >  compile 201300035    Static hostname: orz          Icon name: computer-laptop            Chassis: laptop         Machine ID: a571b61377bb442e9b3fad100b7c407f            Boot ID: c3e3e4a3f5f34ab08f5fd2063035fb19   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  23:38:27 up  4:10,  4 users,  load average: 2.21, 1.83, 1.69

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -44,6 +44,7 @@ void do_syscall(Context *c) {
         }
         case SYS_read: {
             // int _read(int fd, void *buf, size_t count)
+            assert(a[2] != 0);
             c->GPRx = fs_read(a[1], (void *)a[2], a[3]);
 #ifdef ENABLE_STRACE
             printf("[strace] %s(\"%s\", *buf = %p, len = %d) = %d\n",
