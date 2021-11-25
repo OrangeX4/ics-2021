e3340e4d28315585b7c6e886996af61545c5b268
tracer-ics2021 <tracer@njuics.org>
2021-11-25 15:31:59 +0800

    >  compile 201300035    Static hostname: orz          Icon name: computer-laptop            Chassis: laptop         Machine ID: a571b61377bb442e9b3fad100b7c407f            Boot ID: 6042e166ae2e4055a028df2c88bbee14   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  15:31:59 up  1:36,  4 users,  load average: 1.92, 1.50, 1.48

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -65,8 +65,8 @@ int NDL_QueryAudio() {
 int NDL_Init(uint32_t flags) {
   if (getenv("NWM_APP")) {
     evtdev = 3;
+    assert(0);
   }
-  assert(0);
   fp_event = open("/dev/events", "r+");
   return 0;
 }
