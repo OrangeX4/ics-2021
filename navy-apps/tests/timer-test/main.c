1887763f240aefb556af05b5af4b30b69acd6360
tracer-ics2021 <tracer@njuics.org>
2021-11-25 09:09:06 +0800

    >  compile 201300035    Static hostname: orz          Icon name: computer-laptop            Chassis: laptop         Machine ID: a571b61377bb442e9b3fad100b7c407f            Boot ID: 0adc5243901b40e68e9140f0ad209c3a   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  09:09:06 up 24 min,  4 users,  load average: 2.10, 1.92, 1.38

---
+++ navy-apps/tests/timer-test/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <sys/_timeval.h>
+
+#define true 1
+
+// extern struct timeval;
+
+// extern struct timezone;
+
+int _gettimeofday(struct timeval *tv, struct timezone *tz);
+
+int main() {
+  struct timeval tv = {};
+  _gettimeofday(&tv, NULL);
+  long lst_sec = tv.tv_sec;
+  long cur_sec = tv.tv_sec;
+  while (true) {
+    _gettimeofday(&tv, NULL);
+    cur_sec = tv.tv_sec;
+    if (cur_sec - lst_sec >= 30) {
+        printf("Hello, world!\n");
+        lst_sec = cur_sec;
+    }
+  }
+  return 0;
+}
