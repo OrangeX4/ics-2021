db41fba2ff166d4190eaf7f7085cb9de559bca8c
tracer-ics2021 <tracer@njuics.org>
2021-11-25 11:28:20 +0800

    >  compile 201300035    Static hostname: orz          Icon name: computer-laptop            Chassis: laptop         Machine ID: a571b61377bb442e9b3fad100b7c407f            Boot ID: 0adc5243901b40e68e9140f0ad209c3a   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  11:28:20 up  2:43,  4 users,  load average: 2.80, 3.01, 2.56

--- navy-apps/tests/event-test/main.c
+++ navy-apps/tests/event-test/main.c
@@ -6,8 +6,8 @@ int main() {
   NDL_Init(0);
   while (1) {
     char buf[64];
-    assert(0);
     if (NDL_PollEvent(buf, sizeof(buf))) {
+      assert(0);
       printf("receive event: %s\n", buf);
     }
   }
