adb97987f9cd78537b9f32cf9a359f8157e25b55
tracer-ics2021 <tracer@njuics.org>
2021-11-25 11:22:57 +0800

    >  compile 201300035    Static hostname: orz          Icon name: computer-laptop            Chassis: laptop         Machine ID: a571b61377bb442e9b3fad100b7c407f            Boot ID: 0adc5243901b40e68e9140f0ad209c3a   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  11:22:58 up  2:38,  4 users,  load average: 3.48, 2.76, 2.27

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -33,11 +33,9 @@ int gettimeofday(struct timeval *tv, struct timezone *tz) {
 
 size_t events_read(void *buf, size_t offset, size_t len) {
   AM_INPUT_KEYBRD_T ev = io_read(AM_INPUT_KEYBRD);
-  if (ev.keycode == AM_KEY_NONE) return 0;
   assert(0);
-  int size = snprintf(buf, len, "%s %s\n", ev.keydown ? "kd" : "ku", keyname[ev.keycode]);
-  printf("Event: %s", buf);
-  return size;
+  if (ev.keycode == AM_KEY_NONE) return 0;
+  return snprintf(buf, len, "%s %s\n", ev.keydown ? "kd" : "ku", keyname[ev.keycode]);
 }
 
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
