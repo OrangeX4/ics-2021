b085c30a344ef7c5c7e675290a129c866b669818
tracer-ics2021 <tracer@njuics.org>
2021-11-26 14:49:24 +0800

    >  compile 201300035    Static hostname: orz          Icon name: computer-laptop            Chassis: laptop         Machine ID: a571b61377bb442e9b3fad100b7c407f            Boot ID: dbbb1bc8089d4e77b43339f84d9364c7   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  14:49:24 up 57 min,  4 users,  load average: 2.27, 1.99, 1.90

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -142,7 +142,9 @@ void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
     long offset = (off_h + y + i) * screen_w + off_w + x;
     printf("offset: %d\n", offset);
     printf("size: %d\n", screen_h * screen_w);
+    printf("fp: %d\n", fp);
     fseek(fp, offset, SEEK_SET);
+    assert(0);
     write(fp, pixels, w);
   }
   close(fp);
