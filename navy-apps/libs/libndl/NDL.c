b49968dc3cf9e3acb3fedf15df6503037748be48
tracer-ics2021 <tracer@njuics.org>
2021-11-26 11:48:02 +0800

    >  compile 201300035    Static hostname: orz          Icon name: computer-laptop            Chassis: laptop         Machine ID: a571b61377bb442e9b3fad100b7c407f            Boot ID: 9bb2bd41900847b190e0d30d94c299a1   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  11:48:02 up  1:37,  4 users,  load average: 1.85, 1.84, 1.75

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -123,8 +123,8 @@ void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
   FILE *fp = open("/dev/fb", "w+");
   long off_h = (screen_h - canvas_h) / 2;
   long off_w = (screen_w - canvas_w) / 2;
-  // printf("screen_h: %d\n", screen_h);
-  // printf("screen_w: %d\n", screen_w);
+  printf("screen_h: %d\n", screen_h);
+  printf("screen_w: %d\n", screen_w);
   // printf("canvas_h: %d\n", canvas_h);
   // printf("canvas_w: %d\n", canvas_w);
   // printf("off_h: %d\n", off_h);
