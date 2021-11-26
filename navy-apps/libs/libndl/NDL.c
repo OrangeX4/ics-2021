5c86e3bac74fa54f0be594eb21809e5e18273c6a
tracer-ics2021 <tracer@njuics.org>
2021-11-26 11:42:01 +0800

    >  compile 201300035    Static hostname: orz          Icon name: computer-laptop            Chassis: laptop         Machine ID: a571b61377bb442e9b3fad100b7c407f            Boot ID: 9bb2bd41900847b190e0d30d94c299a1   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  11:42:01 up  1:31,  4 users,  load average: 2.03, 1.85, 1.71

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -127,12 +127,12 @@ void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
   printf("screen_w: %d\n", screen_w);
   printf("canvas_h: %d\n", canvas_h);
   printf("canvas_w: %d\n", canvas_w);
-  // printf("off_h: %d\n", off_h);
-  // printf("off_w: %d\n", off_w);
-  // printf("x: %d\n", x);
-  // printf("y: %d\n", y);
-  // printf("w: %d\n", w);
-  // printf("h: %d\n", h);
+  printf("off_h: %d\n", off_h);
+  printf("off_w: %d\n", off_w);
+  printf("x: %d\n", x);
+  printf("y: %d\n", y);
+  printf("w: %d\n", w);
+  printf("h: %d\n", h);
   for (int i = 0; i < h; ++i) {
     long offset = (off_h + y + i) * screen_w + off_w + x;
     printf("offset: %d\n", offset);
