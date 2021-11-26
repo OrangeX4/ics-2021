88058e9a264cc9ee935a1b1474d6f6c9fefabac1
tracer-ics2021 <tracer@njuics.org>
2021-11-26 14:07:16 +0800

    >  compile 201300035    Static hostname: orz          Icon name: computer-laptop            Chassis: laptop         Machine ID: a571b61377bb442e9b3fad100b7c407f            Boot ID: dbbb1bc8089d4e77b43339f84d9364c7   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  14:07:16 up 15 min,  4 users,  load average: 2.63, 2.31, 1.38

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -74,7 +74,7 @@ size_t fs_read(int fd, void *buf, size_t len) {
         // finfo->open_offset += len;
     } else {
         // Normal File
-        assert(finfo->open_offset <= finfo->size);
+        assert(finfo->open_offset + len <= finfo->size);
         ramdisk_read(buf, finfo->disk_offset + finfo->open_offset, len);
         finfo->open_offset += len;
         return len;
@@ -93,7 +93,7 @@ size_t fs_write(int fd, const void *buf, size_t len) {
         // finfo->open_offset += len;
     } else {
         // Normal File
-        assert(finfo->open_offset <= finfo->size);
+        assert(finfo->open_offset + len <= finfo->size);
         ramdisk_write(buf, finfo->disk_offset + finfo->open_offset, len);
         finfo->open_offset += len;
         return len;
