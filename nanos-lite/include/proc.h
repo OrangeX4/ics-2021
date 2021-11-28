ff6733132f56a3ee65c1b348dd724b5d26c05a97
tracer-ics2021 <tracer@njuics.org>
2021-11-28 15:27:57 +0800

    >  compile 201300035    Static hostname: orz          Icon name: computer-laptop            Chassis: laptop         Machine ID: a571b61377bb442e9b3fad100b7c407f            Boot ID: 3972e44781dc4b69a941db5d181a4756   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  15:27:57 up 39 min,  3 users,  load average: 2.93, 2.69, 2.25

--- nanos-lite/include/proc.h
+++ nanos-lite/include/proc.h
@@ -18,10 +18,10 @@ typedef union {
 
 extern PCB *current;
 
-#define ENTRY_PROGRAM "/bin/menu"
+// #define ENTRY_PROGRAM "/bin/menu"
 // #define ENTRY_PROGRAM "/bin/nterm"
 // #define ENTRY_PROGRAM "/bin/nslider"
-// #define ENTRY_PROGRAM "/bin/bmp-test"
+#define ENTRY_PROGRAM "/bin/bmp-test"
 
 
 #endif
