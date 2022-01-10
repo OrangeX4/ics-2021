06f3ff88ceab6c6cfe57cdfbb212ccd4705ea2f1
tracer-ics2021 <tracer@njuics.org>
2022-01-10 19:25:36 +0800

    >  compile 201300035    Static hostname: orz          Icon name: computer-laptop            Chassis: laptop         Machine ID: a571b61377bb442e9b3fad100b7c407f            Boot ID: d350e40790ea4d03a6966c9ee5aa0950   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-44-generic       Architecture: x86-64  19:25:36 up  3:45,  4 users,  load average: 2.25, 1.39, 1.19

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -29,11 +29,11 @@ void context_kload(PCB* pcb, void (*entry)(void *), void *arg) {
 
 void context_uload(PCB* pcb, const char *filename) {
   Area kstack = { (void *) pcb, (void *) pcb + sizeof(PCB) };
-  Context *c = (Context*)kstack.end - 1;
-  c->GPRx = (uintptr_t)heap.end;
   // printf("addr: %p\n", c->GPRx);
   // printf("entry: %p\n", pcb_uload(pcb, filename));
   pcb->cp = ucontext(&pcb->as, kstack, (void *) pcb_uload(pcb, filename));
+  Context *c = (Context*)kstack.end - 1;
+  c->GPRx = (uintptr_t)heap.end;
 }
 
 void init_proc() {
