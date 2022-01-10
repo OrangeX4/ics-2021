4b814b0aa3442e78da8bd8b77f66575dc8acf35b
tracer-ics2021 <tracer@njuics.org>
2022-01-10 23:52:29 +0800

    >  compile 201300035    Static hostname: orz          Icon name: computer-laptop            Chassis: laptop         Machine ID: a571b61377bb442e9b3fad100b7c407f            Boot ID: f21cb60efb7548f2978e5073ab00e055   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-44-generic       Architecture: x86-64  23:52:29 up 38 min,  4 users,  load average: 2.07, 1.58, 1.47

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -27,14 +27,67 @@ void context_kload(PCB* pcb, void (*entry)(void *), void *arg) {
   pcb->cp = kcontext(kstack, entry, arg);
 }
 
-void context_uload(PCB* pcb, const char *filename) {
+#define MAX_STRINGS_LEN 10000
+
+// 返回字符串数组的元素个数
+static int strings_len(char *const *strings) {
+  int count = 0;
+  for (; *strings != NULL; ++strings) ++count;
+  return count;
+}
+
+// 返回字符串数组的对齐占用的空间大小
+static int strings_size(char *const *strings, int align) {
+  int size = 0;
+  for (; *strings != NULL; ++strings) size += (strlen(*strings) / align + 1);
+  return size;
+}
+
+// 复制字符串数组
+static void strings_copy(char *const *strings, char **new_strings, char *buf, int align) {
+  for (; *strings != NULL; ++strings, ++new_strings) {
+    *new_strings = *strings;
+    strcpy(buf, *strings);
+    buf += (strlen(*strings) / align + 1) * align;
+  }
+  *new_strings = NULL;
+}
+
+void context_uload(PCB *pcb, const char *filename, char *const argv[], char *const envp[]) {
   Area kstack = { (void *) pcb, (void *) pcb + sizeof(PCB) };
   pcb->cp = ucontext(&pcb->as, kstack, (void *) pcb_uload(pcb, filename));
   Context *c = (Context*)kstack.end - 1;
-  c->GPRx = (uintptr_t)heap.end;
+  
+  // 当前默认栈顶为 heap.end
+  uintptr_t *ustack = (uintptr_t *)heap.end;
+
+  // 向下生成字符串区等栈区所需数据, 还要注意对齐
+  int argc = strings_len(argv);
+  int envc = strings_len(envp);
+  int arg_size = strings_size(argv, sizeof(uintptr_t));
+  int env_size = strings_size(envp, sizeof(uintptr_t));
+
+  // 栈顶自减
+  ustack -= (env_size + arg_size + envc + 1 + argc + 1);
+
+  // 各个地址
+  int *argc_p = (int *) (ustack);
+  char **argv_p = (char **) (ustack + 1);
+  char **envp_p = (char **) (ustack + 1 + argc + 1);
+  char *arg_str = (char *) (ustack + 1 + argc + 1 + envc + 1);
+  char *env_str = (char *) (ustack + 1 + argc + 1 + envc + 1 + arg_size);
+
+  // 写入数据
+  *argc_p = argc;
+  strings_copy(argv, argv_p, arg_str, sizeof(uintptr_t));
+  strings_copy(envp, envp_p, env_str, sizeof(uintptr_t));
+
+  c->GPRx = (uintptr_t)ustack;
 }
 
 void init_proc() {
+
+  char *const empty[] =  { NULL };
 
   Log("Initializing processes...");
 
@@ -42,7 +95,7 @@ void init_proc() {
   context_kload(&pcb[0], hello_fun, "&pcb[0]");
   // context_kload(&pcb[1], hello_fun, "&pcb[1]");
   // context_uload(&pcb[1], "/bin/pal");
-  context_uload(&pcb[1], "/bin/hello");
+  context_uload(&pcb[1], "/bin/hello", empty, empty);
   switch_boot_pcb();
 
   // load program here
--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -5,16 +5,22 @@
 int main(int argc, char *argv[], char *envp[]);
 extern char **environ;
 void call_main(uintptr_t *args) {
-  char *empty[] =  {NULL };
+  char *empty[] =  { NULL };
 
-  // int argc = *(int *)args;
-  // char **argv = (char **)(args + 1);
-  // while (args != NULL) ++args;
-  // char **envp = (char **)(args + 1);
+  int argc = *(int *)args;
+  char **argv = (char **)(args + 1);
+  while (args != NULL) ++args;
+  char **envp = (char **)(args + 1);
 
-  int argc = 0;
-  char **argv = empty;
-  char **envp = empty;
+  // int argc = 0;
+  // char **argv = empty;
+  // char **envp = empty;
+
+  assert(argc == 0);
+  assert(*argv == NULL);
+  assert(*envp == NULL);
+
+  assert(0);
 
   environ = envp;
   exit(main(argc, argv, envp));
