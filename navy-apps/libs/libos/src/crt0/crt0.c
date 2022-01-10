c0c0482b905d1fa206c4a790686bafb85e97aaea
tracer-ics2021 <tracer@njuics.org>
2022-01-11 00:10:30 +0800

    >  compile 201300035    Static hostname: orz          Icon name: computer-laptop            Chassis: laptop         Machine ID: a571b61377bb442e9b3fad100b7c407f            Boot ID: f21cb60efb7548f2978e5073ab00e055   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-44-generic       Architecture: x86-64  00:10:31 up 56 min,  4 users,  load average: 1.88, 1.39, 1.43

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -9,18 +9,18 @@ void call_main(uintptr_t *args) {
   char *empty[] =  { NULL };
 
   int argc = *(int *)args;
-  assert(argc == 0);
   char **argv = (char **)(args + 1);
-  assert(*argv == NULL);
-  assert(0);
-  while (args != NULL) ++args;
+  while (*args != NULL) ++args;
   char **envp = (char **)(args + 1);
-  assert(*envp == NULL);
 
   // int argc = 0;
   // char **argv = empty;
   // char **envp = empty;
   
+  assert(argc == 0);
+  assert(*argv == NULL);
+  assert(*envp == NULL);
+  assert(0);
 
   environ = envp;
   exit(main(argc, argv, envp));
