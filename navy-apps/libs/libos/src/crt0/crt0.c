7725397d9522a267d07f468ca2b562695cfa3eb1
201300035-Fang Shengjun <201300035@smail.nju.edu.cn>
2022-01-11 00:24:48 +0800

    Complete 2/3 of PA4.1

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -5,26 +5,25 @@
 int main(int argc, char *argv[], char *envp[]);
 extern char **environ;
 void call_main(uintptr_t *args) {
-  printf("args: %p\n", args);
-  char *empty[] =  { NULL };
 
   int argc = *(int *)args;
   char **argv = (char **)(args + 1);
   while (*args != NULL) ++args;
   char **envp = (char **)(args + 1);
 
+  // char *empty[] =  { NULL };
   // int argc = 0;
   // char **argv = empty;
   // char **envp = empty;
 
-  printf("argc: %d\n", argc);
-  printf("argv[0]: %s\n", argv[0]);
-  printf("argv[1]: %s\n", argv[1]);
-  assert(argv[2] == NULL);
-  printf("envp[0]: %s\n", envp[0]);
-  assert(envp[1] == NULL);
-  
-  assert(0);
+  // printf("args: %p\n", args);
+  // printf("argc: %d\n", argc);
+  // printf("argv[0]: %s\n", argv[0]);
+  // printf("argv[1]: %s\n", argv[1]);
+  // assert(argv[2] == NULL);
+  // printf("envp[0]: %s\n", envp[0]);
+  // assert(envp[1] == NULL);
+  // assert(0);
 
   environ = envp;
   exit(main(argc, argv, envp));
