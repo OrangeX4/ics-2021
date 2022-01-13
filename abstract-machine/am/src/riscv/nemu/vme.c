30917c14e45fd1d468728795bdcc4616d93ff80f
tracer-ics2021 <tracer@njuics.org>
2022-01-13 23:04:34 +0800

    >  compile 201300035    Static hostname: orz          Icon name: computer-laptop            Chassis: laptop         Machine ID: a571b61377bb442e9b3fad100b7c407f            Boot ID: a975a1f0891f47e89bda594f0c8d397c   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-46-generic       Architecture: x86-64  23:04:34 up 10:03,  4 users,  load average: 2.82, 2.23, 1.68

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -27,8 +27,8 @@ static Area segments[] = {      // Kernel memory mappings
 
 static inline void set_satp(void *pdir) {
   uintptr_t mode = 1ul << (__riscv_xlen - 1);
-  assert(0);
   asm volatile("csrw satp, %0" : : "r"(mode | ((uintptr_t)pdir >> 12)));
+  assert(0);
 }
 
 static inline uintptr_t get_satp() {
