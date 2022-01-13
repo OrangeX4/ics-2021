ed8f2a152c5cd9f06c821f928696015a7f8df313
tracer-ics2021 <tracer@njuics.org>
2022-01-14 00:33:29 +0800

    >  compile 201300035    Static hostname: orz          Icon name: computer-laptop            Chassis: laptop         Machine ID: a571b61377bb442e9b3fad100b7c407f            Boot ID: a975a1f0891f47e89bda594f0c8d397c   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-46-generic       Architecture: x86-64  00:33:29 up 11:32,  4 users,  load average: 1.47, 1.52, 1.55

--- nemu/src/isa/riscv32/system/mmu.c
+++ nemu/src/isa/riscv32/system/mmu.c
@@ -2,35 +2,37 @@
 #include <memory/paddr.h>
 #include <memory/vaddr.h>
 
-typedef struct {
-  uintptr_t valid : 1;
-  uintptr_t read : 1;
-  uintptr_t write : 1;
-  uintptr_t execute : 1;
-  uintptr_t other : 6;
-  uintptr_t ppn : 22;
-} RISCV_PTE;
+// typedef struct {
+//   word_t valid : 1;
+//   word_t read : 1;
+//   word_t write : 1;
+//   word_t execute : 1;
+//   word_t other : 6;
+//   word_t ppn : 22;
+// } RISCV_PTE;
 
-#define VA_PPN1x4(va) (((uintptr_t)va >> 22) << 2)
-#define VA_PPN0x4(va) ((((uintptr_t)va & 0x3fffff) >> 12) << 2)
+#define VA_PPN1x4(va) (((word_t)va >> 22) << 2)
+#define VA_PPN0x4(va) ((((word_t)va & 0x3fffff) >> 12) << 2)
+#define VALID(pte) (pte & 1)
+#define PPN(pte) (pte >> 10)
 
 #define PGSIZE 4096
 
 // #define __riscv_xlen 32
 // static inline void set_satp(void *pdir) {
-//   uintptr_t mode = 1ul << (__riscv_xlen - 1);
-//   asm volatile("csrw satp, %0" : : "r"(mode | ((uintptr_t)pdir >> 12)));
+//   word_t mode = 1ul << (__riscv_xlen - 1);
+//   asm volatile("csrw satp, %0" : : "r"(mode | ((word_t)pdir >> 12)));
 // }
 
-static inline uintptr_t get_satp() {
-  uint32_t satp = cpu.csr[4]._32; // satp
+static inline word_t get_satp() {
+  word_t satp = cpu.csr[4]._32; // satp
   return satp << 12;
 }
 
 int isa_mmu_check(vaddr_t vaddr, int len, int type) {
 
-  uintptr_t satp = cpu.csr[4]._32; // satp
-  uintptr_t mode = 1ul << (32 - 1);
+  word_t satp = cpu.csr[4]._32; // satp
+  word_t mode = 1ul << (32 - 1);
   
   if (satp & mode) {
     return MMU_TRANSLATE;
@@ -42,21 +44,19 @@ int isa_mmu_check(vaddr_t vaddr, int len, int type) {
 paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
 
   printf("translate: 0x%x\n", vaddr);
-  printf("get_satp(): 0x%lx\n", get_satp());
+  printf("get_satp(): 0x%x\n", get_satp());
 
-  RISCV_PTE *page_dir_item = (RISCV_PTE *)((uintptr_t)get_satp() + VA_PPN1x4(vaddr)); 
+  word_t page_dir_item = paddr_read((word_t)get_satp() + VA_PPN1x4(vaddr), 4); 
 
-  printf("page_dir_item: %p\n", page_dir_item);
+  printf("page_dir_item: 0x%x\n", page_dir_item);
 
-  assert(page_dir_item->valid);
+  assert(VALID(page_dir_item));
 
-  printf("page_dir_item: %p\n", page_dir_item);
+  word_t page_table_item = paddr_read((PPN(page_dir_item) << 12) + VA_PPN0x4(vaddr), 4);
 
-  RISCV_PTE *page_table_item = (RISCV_PTE *)((page_dir_item->ppn << 12) + VA_PPN0x4(vaddr));
+  assert(VALID(page_table_item));
 
-  assert(page_table_item->valid);
-
-  paddr_t paddr = (paddr_t)((page_table_item->ppn << 12) + (vaddr & 0xfff));
+  paddr_t paddr = paddr_read((PPN(page_table_item) << 12) + (vaddr & 0xfff), 4);
 
   assert(paddr == vaddr);
 
