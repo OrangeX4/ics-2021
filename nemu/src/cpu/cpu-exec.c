ff7e623a0a5b39a9c173ff19394adfb7752b20bb
tracer-ics2021 <tracer@njuics.org>
2022-01-14 00:01:19 +0800

    >  compile 201300035    Static hostname: orz          Icon name: computer-laptop            Chassis: laptop         Machine ID: a571b61377bb442e9b3fad100b7c407f            Boot ID: a975a1f0891f47e89bda594f0c8d397c   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-46-generic       Architecture: x86-64  00:01:19 up 11:00,  4 users,  load average: 2.14, 1.43, 1.31

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -101,11 +101,6 @@ void assert_fail_msg() {
 }
 
 void fetch_decode(Decode *s, vaddr_t pc) {
-    if (pc == 0x80001a08) {
-      printf("break, pc: 0x%x\n", pc);
-      printf("break, pc: 0x%x\n", pc);
-      printf("break, pc: 0x%x\n", pc);
-    }
     s->pc = pc;
     s->snpc = pc;
     int idx = isa_fetch_decode(s);
