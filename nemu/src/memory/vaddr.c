e130afa706a00956fa1c411cfdab889ae28baff9
tracer-ics2021 <tracer@njuics.org>
2022-01-14 17:51:26 +0800

    >  compile 201300035    Static hostname: orz          Icon name: computer-laptop            Chassis: laptop         Machine ID: a571b61377bb442e9b3fad100b7c407f            Boot ID: c8d31defdca343adba8c781612edd4ba   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-46-generic       Architecture: x86-64  17:51:26 up  5:19,  4 users,  load average: 1.55, 1.78, 1.67

--- nemu/src/memory/vaddr.c
+++ nemu/src/memory/vaddr.c
@@ -3,7 +3,6 @@
 
 word_t vaddr_ifetch(vaddr_t addr, int len) {
   if (isa_mmu_check(addr, len, 0) == MMU_TRANSLATE) {
-    assert(addr != 0x400657d8);
     return paddr_read(isa_mmu_translate(addr, len, 0), len);
   } else {
     return paddr_read(addr, len);
@@ -12,7 +11,6 @@ word_t vaddr_ifetch(vaddr_t addr, int len) {
 
 word_t vaddr_read(vaddr_t addr, int len) {
   if (isa_mmu_check(addr, len, 0) == MMU_TRANSLATE) {
-    assert(addr != 0x400657d8);
     return paddr_read(isa_mmu_translate(addr, len, 0), len);
   } else {
     return paddr_read(addr, len);
@@ -22,7 +20,6 @@ word_t vaddr_read(vaddr_t addr, int len) {
 void vaddr_write(vaddr_t addr, int len, word_t data) {
   assert(len <= 4096);
   if (isa_mmu_check(addr, len, 0) == MMU_TRANSLATE) {
-    assert(addr != 0x400657d8);
     paddr_write(isa_mmu_translate(addr, len, 0), len, data);
   } else {
     paddr_write(addr, len, data);
