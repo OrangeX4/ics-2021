15f7dac83afd1fa33d2a5c90ab5cdc66a4f23690
tracer-ics2021 <tracer@njuics.org>
2022-01-15 15:18:28 +0800

    >  compile 201300035    Static hostname: orz          Icon name: computer-laptop            Chassis: laptop         Machine ID: a571b61377bb442e9b3fad100b7c407f            Boot ID: ee824708f6f54c95b1f786f30357de73   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-46-generic       Architecture: x86-64  15:18:28 up  3:15,  5 users,  load average: 1.87, 1.68, 1.61

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -9,9 +9,6 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
     cpu.csr[3]._32 = epc;    // mepc
     cpu.csr[1]._32 = NO;     // mcause
 
-    if (NO == IRQ_TIMER) {
-      printf("isa_raise_intr\n");
-    }
 
     // 将 mstatus.MIE 保存到 mstatus.MPIE 中,
     // 然后将 mstatus.MIE 位置为 0
@@ -24,6 +21,7 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
 log_write("[etrace] mcause: %d, mstatus: %x, mepc: %x\n", cpu.csr[1]._32, cpu.csr[2]._32, cpu.csr[3]._32);
 #endif
 
+    printf("mtvec: 0x%x", cpu.csr[0]._32);
     return cpu.csr[0]._32;          // mtvec
 }
 
