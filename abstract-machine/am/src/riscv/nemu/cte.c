c38d99ef73fe8d04f6e9e7bd7ffda4642f2050b5
tracer-ics2021 <tracer@njuics.org>
2022-01-15 15:48:02 +0800

    >  compile 201300035    Static hostname: orz          Icon name: computer-laptop            Chassis: laptop         Machine ID: a571b61377bb442e9b3fad100b7c407f            Boot ID: ee824708f6f54c95b1f786f30357de73   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-46-generic       Architecture: x86-64  15:48:02 up  3:45,  5 users,  load average: 2.14, 1.97, 1.86

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -25,7 +25,6 @@ Context* __am_irq_handle(Context *c) {
       }
       case IRQ_TIMER: {
         ev.event = EVENT_IRQ_TIMER;
-        c->mepc -= 4;
         break;
       }
       default: ev.event = EVENT_ERROR; break;
