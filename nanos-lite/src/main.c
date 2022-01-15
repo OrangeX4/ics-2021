5d00153fd6d4ce33fbecceb51989862b1d41aeef
tracer-ics2021 <tracer@njuics.org>
2022-01-15 15:23:40 +0800

    >  compile 201300035    Static hostname: orz          Icon name: computer-laptop            Chassis: laptop         Machine ID: a571b61377bb442e9b3fad100b7c407f            Boot ID: ee824708f6f54c95b1f786f30357de73   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-46-generic       Architecture: x86-64  15:23:40 up  3:20,  5 users,  load average: 2.16, 1.92, 1.73

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -12,10 +12,10 @@ int main() {
   printf("%s", logo);
   Log("'Hello World!' from Nanos-lite");
   Log("Build time: %s, %s", __TIME__, __DATE__);
-  assert(0);
 
   init_mm();
 
+  assert(0);
   init_device();
 
   init_ramdisk();
